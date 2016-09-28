801bb2326fa9a467d3aedecafe1209b16acc9e34
Stacey Burns <staceyburns@protonmail.com>
2016-09-28 10:32:52 +0200

    Update syntax.js

--- syntax.js
+++ syntax.js
@@ -1,11 +1,1 @@
-const isCssImage = val = /^url\(["']?http/.test(val) ;
-
-function something() {
- 
-}
-
-//
-
-//
-
-// add another commit after PR is created
+// code showing in 1st line. Testing scrollbar.....................................................................................................................................................
