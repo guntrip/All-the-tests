5e0fc2dee2b58250ec11d454b782789f38f37b39
Stacey Burns <staceyburns@protonmail.com>
2016-12-14 14:41:43 +0100

    `times` を使ってテストを修正

--- firstbranch.js
+++ firstbranch.js
@@ -4,10 +4,3 @@ vvvv
 //test for tabs
      test for tabs
           test for tabs \
-      ,,,,
-           
-           
-           lllll
-=======
-object member
->>>>>>> cf3868eb8225469a7d75165559ec6fd4a2c3b2af
