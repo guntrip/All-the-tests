60b81bef72c957333e0db2ffb500f8c21656ae6e
Stacey Burns <staceyburns@protonmail.com>
2016-12-14 16:47:28 +0100

    Update syntax.js

--- syntax.js
+++ syntax.js
@@ -1,16 +1,1 @@
-const isCssImage = val = /^url\(["']?http/.test(val) ;
-
-function something() {
- 
-}
-
-//
-
-<<<<<<< HEAD
-
-//update branch test
-=======
-//
-
-// add another commit after PR is created
->>>>>>> cf3868eb8225469a7d75165559ec6fd4a2c3b2af
+mm
